2c497d78470727f671aed0e7fa09c9bbbd6c1587
poorviagrawal <poorvi.agrawal@nagarro.com>
2024-04-19 15:48:36 +0530

    static page error resolved

--- app/page.tsx
+++ app/page.tsx
@@ -1,57 +1,45 @@
-import getCurrentUser from "./actions/getCurrentUser";
-import getListings, { IListingsParams } from "./actions/getListings";
 import ClientOnly from "./components/ClientOnly";
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
 import ListingCard from "./components/listings/ListingCard";
+import { Listing } from "@prisma/client";
+import { GetServerSidePropsContext } from "next";
+import getListings, { IListingsParams } from "./actions/getListings";
+import getCurrentUser from "./actions/getCurrentUser";
+import { SafeUser } from "./types";
 
 interface HomeProps {
-  searchParams: IListingsParams;
+  listings: Listing[];
+  currentUser?: SafeUser | null;
 }
 
-const Home = async ({searchParams} : HomeProps) => {
-
-  const listings = await getListings(searchParams);
-  const currentUser = await getCurrentUser();
-
-  if(listings.length === 0) {
+const Home = ({ listings, currentUser }: HomeProps) => {
+  if (listings.length === 0) {
     return (
       <ClientOnly>
-        <EmptyState
-          showReset
-        />
+        <EmptyState showReset />
       </ClientOnly>
-    )
+    );
   }
 
   return (
     <ClientOnly>
       <Container>
-        <div className="
-         pt-24
-         grid
-         grid-cols-1
-         sm:grid-cols-2
-         md:grid-cols-4
-         lg:grid-cols-5
-         2xl:grid-cols-6
-         gap-8
-        ">
-          {listings.map((listing) => {
-            return (
-              <ListingCard
-                currentUser = {currentUser}
-                key = {listing.id}
-                data = {listing}
-              />
-            )
-          })}
-
+        <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 2xl:grid-cols-6 gap-8">
+          {listings.map((listing) => (
+            <ListingCard
+              currentUser={currentUser}
+              key={listing.id}
+              data={{
+                ...listing,
+                createdAt: listing.createdAt.toString(),
+              }}
+            />
+          ))}
         </div>
-
       </Container>
     </ClientOnly>
   );
-}
+};
 
 export default Home;
